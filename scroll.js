bab63fd22180f90c1901275108a1bf2429edba8f
hiroppe <106125124+hiroppe@users.noreply.github.com>
2023-05-13 23:32:23 +0900

    Delete scroll.js

--- scroll.js
+++
@@ -1,8 +0,0 @@
-// アニメーション設定
-const elem = $('#comment');
-elem.addClass("animate__animated");
-elem.addClass("animate__fadeInLeft");
-elem.on("animationend", ()=>{
-	elem.off("animationend");
-	elem.removeAttr("class");
-});
