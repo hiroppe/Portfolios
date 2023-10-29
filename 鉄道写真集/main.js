57fa37acf880707256ac778cb63196c7d4e791d5
hiroppe <106125124+hiroppe@users.noreply.github.com>
2023-10-29 11:50:12 +0900

    Add files via upload

---
+++ 鉄道写真集/main.js
@@ -0,0 +1,7 @@
+console.log("main.js!!");
+window.onload = () => {
+    lightbox.option({
+        'wrapAround': true,//グループ最後の写真の矢印をクリックしたらグループ最初の写真に戻る
+        'albumLabel': ' %1 / total %2 '//合計枚数中現在何枚目かというキャプションの見せ方を変更できる
+    })
+};
